49081ab2eccb6abc12ebbb9295344602b477a5a7
Arseny Tsivarev <me@tsivx.ru>
2025-09-24 13:59:39 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { Heart } from 'lucide-react';
 export default function Footer() {
   return (
     <div className="w-full text-center text-xs text-muted-foreground p-2 items-center flex justify-center gap-1">
-      made with <Heart className="h-4 w-4" /> by @tsivxrev
+      made with <Heart className="h-4 w-4" /> by @tsivx
     </div>
   );
 }
